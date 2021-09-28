626057db4276421ade3b011b956351f04e875cba
kizzteee <58609038+kizzteee@users.noreply.github.com>
2021-09-28 20:34:11 +0100

    initial commit

--- ProductManage/settings.py
+++ ProductManage/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+
+
+import os
 import dj_database_url
 import django_heroku
 
